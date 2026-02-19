860ed0223be9be41dd046b29d969e32fce0ffcfa
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-02-19 16:01:45 +0100

    luna

--- lab02-animacion/lab02-animacion.py
+++ lab02-animacion/lab02-animacion.py
@@ -1,4 +1,5 @@
 import arcade
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,8 +11,13 @@ class MiJuego(arcade.Window):
     
     def on_draw(self):
         self.clear()
-        
-        
+
+        arcade.draw_circle_filled(140, 470, 90, arcade.color.ASH_GREY)
+        for i in range (20):
+
+            puntos += [(random.randint(0, 800), random.randint(0, 600))]
+
+        arcade.draw_points(puntos, arcade.color.WHITE, 5)
 
         
 

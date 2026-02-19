ab77c1cce88adf647b15b92ad0a70cc9b7458e05
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-02-19 14:34:32 +0100

    cositas

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -3,7 +3,7 @@ import arcade
 WIDTH = 800
 HEIGHT = 600
 
-arcade.open_window(WIDTH, HEIGHT, "Example")
+arcade.open_window(WIDTH, HEIGHT, "Cositas")
 
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)

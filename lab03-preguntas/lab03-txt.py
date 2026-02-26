da9cadcdfce25721d71f91348c48fb50a461eb78
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-02-26 15:53:23 +0100

    diccionario creado

--- lab03-preguntas/lab03-txt.py
+++ lab03-preguntas/lab03-txt.py
@@ -1,9 +1,21 @@
-with open ("preguntas.txt", "r", encoding = "utf-8") as file: #abrir el archivo 
+with open ("lab03-preguntas/preguntas.txt", "r", encoding = "utf-8") as file: #abrir el archivo 
 
     for linea in file: #se recorre linea por linea
         print (linea.strip()) #quita el salto de linea
 
-        
 
+def extraer_pregunta (pregunta: str) -> dict:
+    
+    lista = []
+    lista += pregunta.split("|")
+
+   return {"pregunta": "lista[0]",
+           
+           "correcta": "lista[1]",
+           
+           "opciones": "lista[2:]"}
+
+
+    
 
 

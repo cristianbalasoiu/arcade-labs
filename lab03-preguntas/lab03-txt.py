9fe835f78610d3d06dcfa9e02341b9c35056960e
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-02-26 15:36:08 +0100

    inicio preguntas .txt

---
+++ lab03-preguntas/lab03-txt.py
@@ -0,0 +1,9 @@
+with open ("preguntas.txt", "r", encoding = "utf-8") as file: #abrir el archivo 
+
+    for linea in file: #se recorre linea por linea
+        print (linea.strip()) #quita el salto de linea
+
+        
+
+
+

60b1fd8298935ef05ce0dcb6b44db295640f5266
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-04-28 23:54:10 +0200

    botones mas o menos

--- MENU/menu.py
+++ MENU/menu.py
@@ -1,18 +1,20 @@
 import arcade
 from boton import Boton
 
+
+
 class MenuPrincipal(arcade.View):
 
     def __init__(self):
         super().__init__()
         self.botones = []
-        arcade.set_background_color(arcade.color.DARK_RED)
+        arcade.set_background_color(arcade.color.CAMOUFLAGE_GREEN)
         
 
     def on_show(self):
 
-        ancho = self.window.width
-        alto = self.window.height
+        ancho = 800
+        alto = 600
 
         self.botones = [
             Boton(ancho/2 - 150, alto/2 + 50, 300, 60, "JUGAR"),
@@ -22,7 +24,6 @@ class MenuPrincipal(arcade.View):
 
     def on_draw(self):
         self.clear()
-
 
         # Título
         arcade.draw_text(
@@ -34,6 +35,35 @@ class MenuPrincipal(arcade.View):
             anchor_x="center",
             font_name="Arial"
         )
+
+
+
+       # DESPLAZAMIENTO GENERAL HACIA ABAJO
+        offset_y = self.window.height * 0.05
+
+        # BOTÓN 1 (JUGAR)
+        arcade.draw_lrbt_rectangle_filled(
+        self.window.width/2 - self.window.width*0.25,
+        self.window.width/2 + self.window.width*0.25,
+        self.window.height/2 + self.window.height*0.15 - self.window.height*0.06 - self.window.height * 0.05,
+        self.window.height/2 + self.window.height*0.15 + self.window.height*0.06 - self.window.height * 0.05,
+        arcade.color.CHARLESTON_GREEN)
+
+        # BOTÓN 2 (HISTORIA)
+        arcade.draw_lrbt_rectangle_filled(
+        self.window.width/2 - self.window.width*0.25,
+        self.window.width/2 + self.window.width*0.25,
+        self.window.height/2 - self.window.height*0.06 - self.window.height * 0.05,
+        self.window.height/2 + self.window.height*0.06 - self.window.height * 0.05,
+        arcade.color.CHARLESTON_GREEN)
+
+        # BOTÓN 3 (AJUSTES)
+        arcade.draw_lrbt_rectangle_filled(
+        self.window.width/2 - self.window.width*0.25,
+        self.window.width/2 + self.window.width*0.25,
+        self.window.height/2 - self.window.height*0.15 - self.window.height*0.06 - self.window.height * 0.05,
+        self.window.height/2 - self.window.height*0.15 + self.window.height*0.06 - self.window.height * 0.05,
+        arcade.color.CHARLESTON_GREEN)
 
         # Botones
         for boton in self.botones:

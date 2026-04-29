33342e0fa9e5c31763cd5faddf2094d960d62412
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-04-30 00:06:46 +0200

    estica menu hecho

--- MENU/menu.py
+++ MENU/menu.py
@@ -8,7 +8,7 @@ class MenuPrincipal(arcade.View):
     def __init__(self):
         super().__init__()
         self.botones = []
-        arcade.set_background_color(arcade.color.CAMOUFLAGE_GREEN)
+        arcade.set_background_color(arcade.color.BLACK_LEATHER_JACKET)
         
 
     def on_show(self):
@@ -25,45 +25,97 @@ class MenuPrincipal(arcade.View):
     def on_draw(self):
         self.clear()
 
+        center_x = self.window.width / 2
+        center_y = self.window.height / 2
+        half_width = self.window.width * 0.30
+        half_height = self.window.height * 0.06
+
         # Título
         arcade.draw_text(
             "MUNDO CAÍDO",
             self.window.width / 2,
             self.window.height - 150,
-            arcade.color.WHITE,
-            60,
+            arcade.color.VENETIAN_RED,
+            70,
             anchor_x="center",
-            font_name="Arial"
+            font_name="Georgia"
         )
 
-
-
-       # DESPLAZAMIENTO GENERAL HACIA ABAJO
-        offset_y = self.window.height * 0.05
-
         # BOTÓN 1 (JUGAR)
+
+        bottom1 = center_y + self.window.height * 0.02 - half_height
+        top1 = center_y + self.window.height * 0.10 + half_height
+
+    
         arcade.draw_lrbt_rectangle_filled(
-        self.window.width/2 - self.window.width*0.25,
-        self.window.width/2 + self.window.width*0.25,
-        self.window.height/2 + self.window.height*0.15 - self.window.height*0.06 - self.window.height * 0.05,
-        self.window.height/2 + self.window.height*0.15 + self.window.height*0.06 - self.window.height * 0.05,
-        arcade.color.CHARLESTON_GREEN)
+        center_x - half_width,
+        center_x + half_width,
+        center_y + self.window.height * 0.02 - half_height,
+        center_y + self.window.height * 0.10 + half_height,
+        arcade.color.SMOKY_BLACK)
+
+        arcade.draw_lrbt_rectangle_outline(center_x - half_width, center_x + half_width, bottom1, top1, arcade.color.VENETIAN_RED, 3)
 
         # BOTÓN 2 (HISTORIA)
+
+        bottom2 = center_y - self.window.height * 0.16 - half_height
+        top2 = center_y - self.window.height * 0.14 + half_height
+
         arcade.draw_lrbt_rectangle_filled(
-        self.window.width/2 - self.window.width*0.25,
-        self.window.width/2 + self.window.width*0.25,
-        self.window.height/2 - self.window.height*0.06 - self.window.height * 0.05,
-        self.window.height/2 + self.window.height*0.06 - self.window.height * 0.05,
-        arcade.color.CHARLESTON_GREEN)
+        center_x - half_width,
+        center_x + half_width,
+        center_y - self.window.height * 0.16 - half_height,
+        center_y - self.window.height * 0.14 + half_height,
+        arcade.color.SMOKY_BLACK)
+
+        arcade.draw_lrbt_rectangle_outline(center_x - half_width, center_x + half_width, bottom2, top2, arcade.color.VENETIAN_RED, 3)
 
         # BOTÓN 3 (AJUSTES)
+
+        bottom3 = center_y - self.window.height * 0.35 - half_height
+        top3 = center_y - self.window.height * 0.32 + half_height
+
         arcade.draw_lrbt_rectangle_filled(
-        self.window.width/2 - self.window.width*0.25,
-        self.window.width/2 + self.window.width*0.25,
-        self.window.height/2 - self.window.height*0.15 - self.window.height*0.06 - self.window.height * 0.05,
-        self.window.height/2 - self.window.height*0.15 + self.window.height*0.06 - self.window.height * 0.05,
-        arcade.color.CHARLESTON_GREEN)
+        center_x - half_width,
+        center_x + half_width,
+        center_y - self.window.height * 0.35 - half_height,
+        center_y - self.window.height * 0.32 + half_height,
+        arcade.color.SMOKY_BLACK)
+
+        arcade.draw_lrbt_rectangle_outline(center_x - half_width, center_x + half_width, bottom3, top3, arcade.color.VENETIAN_RED, 3)
+
+        # TEXTO BOTÓN 1 (JUGAR)
+        arcade.draw_text(
+        "JUGAR",
+        center_x,
+        center_y + self.window.height * 0.06,  # centro del rectángulo 1
+        arcade.color.GOLDENROD,
+        font_size=50,
+        anchor_x="center",
+        anchor_y="center",
+        font_name="Times New Roman")
+
+        # TEXTO BOTÓN 2 (HISTORIA)
+        arcade.draw_text(
+        "HISTORIA",
+        center_x,
+        center_y - self.window.height * 0.15,  # centro del rectángulo 2
+        arcade.color.GOLDENROD,
+        font_size=30,
+        anchor_x="center",
+        anchor_y="center",
+        font_name="Times New Roman")
+
+        # TEXTO BOTÓN 3 (CONTROLES)
+        arcade.draw_text(
+        "CONTROLES",
+        center_x,
+        center_y - self.window.height * 0.335,  # centro del rectángulo 3
+        arcade.color.GOLDENROD,
+        font_size=30,
+        anchor_x="center",
+        anchor_y="center",
+        font_name="Times New Roman")
 
         # Botones
         for boton in self.botones:

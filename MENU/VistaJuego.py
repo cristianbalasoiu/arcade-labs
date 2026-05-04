e18f3fffd357cc028472e13185538deb7ed57faf
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-05-04 19:54:24 +0200

    botones partidas

--- MENU/VistaJuego.py
+++ MENU/VistaJuego.py
@@ -3,7 +3,7 @@ import arcade
 class VistaJuego(arcade.View):
     def on_draw(self):
         self.clear()
-        arcade.draw_text("JUEGO", 400, 300, arcade.color.WHITE, 40, anchor_x="center")
+
 
     # BOTÓN VOLVER
         left = 20
@@ -31,6 +31,43 @@ class VistaJuego(arcade.View):
             anchor_y="center",
             font_name = "Times New Roman"
         )
+        
+        # TEXTO SUPERIOR
+        arcade.draw_text(
+            "Elige que partida quieres jugar", 
+            self.window.width / 2, 
+            self.window.height / 2 + 150, 
+            arcade.color.WHITE, 
+            40, 
+            anchor_x="center"
+        )
+
+        #BOTONES DE PARTIDAS
+        boton_width = 400
+        boton_height = 80
+        center_x = self.window.width / 2
+        y_nueva = self.window.height / 2 + 60
+        y_seguir = self.window.height / 2 - 60
+
+       
+        for y, texto in [(y_nueva, "NUEVA PARTIDA"), (y_seguir, "SEGUIR PARTIDA")]:
+            left_btn = center_x - boton_width / 2
+            right_btn = center_x + boton_width / 2
+            bottom_btn = y - boton_height / 2
+            top_btn = y + boton_height / 2
+
+            arcade.draw_lrbt_rectangle_filled(left_btn, right_btn, bottom_btn, top_btn, arcade.color.SMOKY_BLACK)
+            arcade.draw_lrbt_rectangle_outline(left_btn, right_btn, bottom_btn, top_btn, arcade.color.RED, 3)
+            arcade.draw_text(
+                texto, 
+                center_x, 
+                y, 
+                arcade.color.GOLDENROD, 
+                25,
+                anchor_x="center", 
+                anchor_y="center", 
+                font_name="Times New Roman"
+            )
 
     def on_mouse_press(self, x, y, button, modifiers):
 
